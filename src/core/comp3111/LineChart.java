184ff1166636438e1ce35d8bc132feeee91dacb9
yuchung <yuen.ting.chung@cern.ch>
2018-04-23 14:27:09 +0800

    Modify the LineChart class

--- src/core/comp3111/LineChart.java
+++ src/core/comp3111/LineChart.java
@@ -4,7 +4,7 @@ package core.comp3111;
 /**
  *  The implementation of the LineChart class. 
  *  It store the line chart in javafx.scene.chart.LineChart.
- * 	By default, the first AxisLabel is set to be the x-axis.
+ * 	B
  * 
  * @author YuenTing
  *
@@ -17,6 +17,16 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 public class LineChart extends Chart{
+	/**
+	 * Constructor of the LineChart
+	 * By default, the first AxisLabel is set to be the x-axis.
+	 * 
+	 * @param DataTable
+	 * @param AxisLabels
+	 * @param ChartName
+	 * @throws ChartException
+	 */
+	
 	
 	public LineChart(DataTable DataTable, String[] AxisLabels, String ChartName) throws ChartException{		
 		super(DataTable, AxisLabels, ChartName, "LineChart");
@@ -33,17 +43,38 @@ public class LineChart extends Chart{
 		//Create the line chart from javafx	
 		
 		//Add the xAxis and yAxis to the LineChart
+		String xlabel = AxisLabels[0];
+		DataColumn xdc = DataTable.getCol(xlabel);
+		
 		NumberAxis xAxis = new NumberAxis();
 		NumberAxis yAxis = new NumberAxis();
-		String xlabel = AxisLabels[0];
 		xAxis.setLabel(xlabel);
 		
 		this.chart = new LineChart<Number,Number>(xAxis,yAxis);
+		this.chart.setTitle("");
 		
+		
+		//Define a series for each column
+		for (String AxisLabel: AxisLabels) {
+	        XYChart.Series series = new XYChart.Series();
+	        series.setName(AxisLabel);
+	        DataColumn ydc  = DataTable.getCol(AxisLabel);
+	        SizeOfdc = ydc.getSize();
+	        for (int i = 0; i < SizeOfdc; i++) {
+	        	series.getData().add(new XYChart.Data<Number, Number>(xdc[i], ydc[i]);
+	        }
+	        chart.getData().add(series);
+			
+			
+		}
 		
 	    
 		
 		
+		
+	}
+	
+	public void ShowGraph() {
 		
 	}
 
