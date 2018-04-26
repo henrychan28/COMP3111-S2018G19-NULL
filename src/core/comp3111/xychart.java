f1aa7d6f7a434b67e8ec1a5b02b4e3eda28dc5c3
ytchung <ytchung@ust.hk>
2018-04-26 11:04:54 +0800

    Add showChart(Stage stage) to xychart. For showing chart on screen.

--- src/core/comp3111/xychart.java
+++ src/core/comp3111/xychart.java
@@ -7,6 +7,8 @@ package core.comp3111;
  * @author YuenTing
  */
 import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 
 public class xychart{
@@ -48,22 +50,62 @@ public class xychart{
 		return;
 		
 	}
+	/**
+	 * Show the chart on the screen
+	 * 
+	 * @param stage - pass the Stage stage
+	 * 
+	 */
 	
+	public void showChart(Stage stage) {
+		
+        Scene scene  = new Scene(xychart, 500, 400);
+        stage.setScene(scene);
+        stage.show();	
+		
+	}
+	/**
+	 * get the ChartID
+	 * 
+	 * @return String ChartID
+	 */
 	public String getChartID() {
 		return this.ChartID;
 	}
+	/**
+	 * get the Chart Name
+	 * 
+	 * @return String ChartName
+	 */
 	public String getChartName() {
 		return this.ChartName;
 	}
+	/**
+	 * get the chart type
+	 * 
+	 * @return String ChartType 
+	 */
 	public String getChartType() {
 		return this.ChartType;
 	}
+	/**
+	 * get the data columns name
+	 * 
+	 * @return String[] AxisLabels
+	 */
+	
 	public String[] getAxisLabels() {
 		return this.AxisLabels;
 	}
+	/**
+	 * get the name of the DataTable referred to
+	 * 
+	 * @ return String DataTableName
+	 */
 	public String getDataTableName() {
 		return DataTableName;
 	}
+
 	
 	//Attributes
 	
@@ -71,7 +113,7 @@ public class xychart{
 	protected String DataTableName;
 	protected String[] AxisLabels;
 	protected String ChartName;
-	protected String ChartID; 
+	protected String ChartID;  //unique
 	protected String ChartType;
 	protected XYChart <Number, Number> xychart;
 	
