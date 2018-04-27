59cda718261f31e6974ce993ff5cf03dbf6e5079
ytchung <ytchung@ust.hk>
2018-04-28 04:24:56 +0800

    Add the basic UI for TypeSelection, ViewHistory, LineChartCreation

--- src/ui/comp3111/GenerateChartUI.java
+++ src/ui/comp3111/GenerateChartUI.java
@@ -2,12 +2,26 @@ package ui.comp3111;
 
 import core.comp3111.*;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.layout.HBox;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.Scene;
+import javafx.scene.chart.XYChart;
 import javafx.scene.Group;
+import javafx.scene.layout.Pane;
+
 
 
 import javafx.stage.Stage;
@@ -23,45 +37,240 @@ import javafx.stage.Stage;
 
 public class GenerateChartUI extends Application {
 
+	//Daata Storage
+	private CoreData coreData = new CoreData();
+	private int[] selectedTableIndex = {Constants.EMPTY, Constants.EMPTY};
+	
+	//Attributes: Scene and Stage
+	public static final int SCENE_CHART_NUM = 6;
+	public static final int SCENE_Chart_TYPE_SELECTION = 0;
+	public static final int SCENE_VIEW_HISTORY = 1;
+	public static final int SCENE_LINE_CHART_SELECTION = 2;
+	public static final int SCENE_SCATTER_CHART_SELECTION = 3;
+	public static final int SCENE_DYNAMIC_CHART_SELECTION = 4;
+	public static final int SCENE_SHOW_CHART = 5;
+	public static final String[] SCENE_CHART_TITLES = {
+			"Chart Type Selection", "View History Charts", "Generate New Line Chart", "Generate New Scatter Chart",
+			"Generate New Dynamic Chart", "Show Chart"
+	};
+	private Stage stage = null;
+	private Scene[] scenes = null;
+	
+	// To keep this application more structural,
+	// The following UI components are used to keep references after invoking
+	// createScene()
+	
+	//screen 1: paneChartSelection
+	private Button btHistory, btGenerateNew;
+	private ComboBox cbChartType;
+	private XYChart<Number, Number> chartPreviewChart;
+	private Label lbSelectType;
+	
+	//screen 2, 3, 4, 5: the back button
+	private Button btbackto1;
+	
+	//screen 2: paneViewHistory
+	private TableColumn  colChartType, colChartID;
+	private Button btshow;
+
+
+	//screen 3: paneLineChartSelection
+	private Label lbSelectNewLineChart, lbLineTitle, lbLineXaxis, lbLineYaxis;
+	private Button btLineSave, btLineSaveandPreview;
+	private ComboBox cbLineTitle, cbLineXaxis, cbLineYaxis;
+	
+	//screen 4: paneScatterChartSelection
+	private Label lbSelectNewScatterChart, lbScatterTitle, lbScatterXaxis, lbScatterYaxis, lbScatterCaxis;
+	private Button btScatterSave, btScatterSaveandPreview;
+	private ComboBox cbScatterTitle, cbScatterXaxis, cbScatterYaxis, cbScatterCaxis;
+	
+	//screen 5: paneDynamicChartSelection
+	
+	
+	
+	//screen 6: paneShowChart
+	private Label lbShowChart;
+	private XYChart<Number, Number> chartShowChart;
+	private Button btbackto2; //back to history
+
+	/*
+	public static VBox ChartTypeSelection;
+	public static TableView<xychart> ViewHistory;
+	public static VBox LineChartSelection;
+	public static VBox ScatterChartSelection;
+	public static VBox DyLineChartSelection;
+	
+	*/	
+	//Methods
+	//1. Pane made
+	private Pane paneChartTypeSelection () {
+		//1. title
+		lbSelectType = new Label("Select the Chart Type: ");
+		lbSelectType.setFont(new Font("Arial", 20));
 		
-	@Override
-	public void start(Stage stage) {
+
+		//2. select type
+
+		cbChartType = new ComboBox();
 		
-		//Create Labels, ComboBoxes, buttons
-		Label SelectTypeL = new Label("Select the Chart Type: ");
-		ComboBox<String> ChartTypeCB = new ComboBox<String>();
-		//TODO
+		/*
+		DataTable selectedDataTable = coreData.getDataTable(selectedTableIndex);
 		//if DataTable contains >= 2 Number Data Columns => show LineChart, Dynamic LineChart
-		if (true) {
-			ChartTypeCB.getItems().addAll("Line Chart", "Dynamic Line Chart");
+		if (selectedDataTable.getNumColOfType(DataType.TYPE_NUMBER) >= 2) {
+			cbChartType.getItems().addAll("Line Chart");
 		}
-		//TODO:
 		//if DataTable contains >= 2 Number Data Columns && >= 1 String Data Columns => show ScatterChart
-		if (true) {
-			ChartTypeCB.getItems().add("Scatter Chart");
+		if (selectedDataTable.getNumColOfType(DataType.TYPE_NUMBER) >= 2 && selectedDataTable.getNumColOfType(DataType.TYPE_STRING) >=1) {
+			cbChartType.getItems().add("Scatter Chart");
+		}*/
+		
+		
+		//TODO: dynamic chart
 			
+		//TODO: preview chart type
+		//3. buttons
+		btHistory = new Button("View History");
+		btGenerateNew = new Button("Generate New");
+		HBox Buttons = new HBox(20);
+		Buttons.getChildren().addAll(btHistory, btGenerateNew);
+		Buttons.setAlignment(Pos.CENTER);
+		//Container
+		VBox container = new VBox(20);
+		container.getChildren().addAll(lbSelectType, cbChartType, new Separator(),  Buttons);
+		container.setAlignment(Pos.CENTER);
+		BorderPane pane = new BorderPane();
+		pane.setCenter(container);
+
+		return pane;
+	}
+	private Pane paneViewHistory() {
+
+		//1. 	
+		TableColumn colHistory = new TableColumn("Chart History");
+		colChartType = new TableColumn ("Chart Type");
+		colChartID = new TableColumn("Chart ID");
+		colHistory.getColumns().addAll(colChartType, colChartID);
+		
+		TableView tvhistory = new TableView();
+		tvhistory.getColumns().add(colHistory);
+		
+		//2. 
+		btbackto1 = new Button("Back");
+		btshow = new Button("show");
+		HBox buttons = new HBox(20);
+		buttons.setAlignment(Pos.CENTER);
+		buttons.getChildren().addAll(btbackto1, btshow);
+		
+		VBox container = new VBox(20);
+		container.getChildren().addAll(tvhistory, buttons);		
+		container.setAlignment(Pos.CENTER);
+
+		
+		BorderPane pane = new BorderPane();
+		pane.setCenter(container);
+		return pane;
+	}
+	private Pane paneLineChartSelection() {
+
+		
+		//1: heading 
+		lbSelectNewLineChart = new Label("Select Line Chart Setting");
+		//2: Title - default DataTable Name, use editable combobox
+		HBox Title = new HBox();
+		lbLineTitle = new Label("Title"); 
+		cbLineTitle = new ComboBox();
+		Title.getChildren().addAll(lbLineTitle, cbLineTitle);
+		//TODO: editable combobox
+
+		
+		//3: x-axis 
+		HBox Xaxis = new HBox();
+		lbLineXaxis = new Label("x-axis");
+		cbLineXaxis = new ComboBox();
+		Xaxis.getChildren().addAll(lbLineXaxis, cbLineYaxis);
+		//TODO: add the key of all number type data columns of the DataTable to the ComboBox
+
+		//4: y-axis
+		HBox Yaxis = new HBox();
+		lbLineYaxis = new Label("y-axis");
+		cbLineYaxis= new ComboBox();
+		Yaxis.getChildren().addAll(lbLineXaxis, cbLineYaxis);
+
+		//TODO: add the key of all number type data columns of the DataTable to the ComboBox
+
+		//5 Buttons
+		HBox ButtonsSave = new HBox();
+		btLineSave = new Button("View History");
+		btLineSaveandPreview= new Button("Generate New");
+		ButtonsSave.getChildren().addAll(btLineSave, btLineSaveandPreview);
+		
+		VBox container = new VBox();
+		container.getChildren().addAll(lbSelectNewLineChart,  Title, Xaxis, Yaxis, ButtonsSave);
+		container.setAlignment(Pos.CENTER);
+		BorderPane pane = new BorderPane();
+		pane.setCenter(container);
+		
+		return pane;
+	}
+	
+	
+	
+	
+	
+	
+	private void initScenes() {
+		scenes = new Scene[SCENE_CHART_NUM];
+		scenes[SCENE_Chart_TYPE_SELECTION] = new Scene(paneChartTypeSelection(), 400, 600);
+/*
+		public static final int SCENE_SCATTER_CHART_SELECTION = 3;
+		public static final int SCENE_DYNAMIC_CHART_SELECTION = 4;
+		public static final int SCENE_SHOW_CHART = 5;*/
+		scenes[SCENE_VIEW_HISTORY] = new Scene(paneViewHistory(), 400, 600);
+		scenes[SCENE_LINE_CHART_SELECTION] = new Scene( paneLineChartSelection() , 400, 600);
+		
+		for(Scene s: scenes) {
+			if(s!= null) {s.getStylesheets().add("Main.css");}
 		}
-		
-		Button BackButton = new Button("Back");
-		Button NextButton = new Button("Next");
-		
-		//Create grid
-		GridPane grid = new GridPane();
-		grid.setVgap(4);
-		grid.setHgap(10);
-		grid.add(SelectTypeL, 0, 0);
-		grid.add(ChartTypeCB, 0, 1);
-		grid.add(BackButton, 1, 2);
-		grid.add(NextButton, 2, 2);
-		
-		Scene scene = new Scene(new Group(), 400, 350);
-		
-		((Group)scene.getRoot()).getChildren().add(grid);
-		stage.setScene(scene);
+	}
+	
+	public void initEventHandlers() {
+		return;
+	}
+	
+	
+	private void putSceneOnStage(int sceneID) {
+
+		// ensure the sceneID is valid
+		if (sceneID < 0 || sceneID >= SCENE_CHART_NUM)
+			return;
+
+		stage.hide();
+		stage.setTitle(SCENE_CHART_TITLES[sceneID]);
+		stage.setScene(scenes[sceneID]);
+		stage.setResizable(true);
 		stage.show();
-			
+	}
+	
+	@Override
+	public void start(Stage primarystage) {
+		 stage = primarystage;
+		 initScenes();
+		 initEventHandlers();
+		 putSceneOnStage(SCENE_LINE_CHART_SELECTION);
+		 
+		
+
+		/*VBox for Scatter Chart Selection UI */
+
+		/*VBox for Dynamic Chart Selection UI */
+
 		
 		
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 	
 }
